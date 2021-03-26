992c00779e96c054ea130839e36c2faa4159bb32
Diganta <ghoshdiganta226@gmail.com>
2021-03-26 23:32:36 +0530

    Login and Signup added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from './Search';
 import Home from './Home'
+import Signup from "./Signup";
+import Login from "./Login";
 function App() {
   return (
     <>
@@ -13,7 +15,13 @@ function App() {
         </Route>
 
          <Route path="/search">
-         < Search />
+         <Search />
+         </Route>
+         <Route path="/signup">
+         < Signup />
+         </Route>
+         <Route path="/login">
+         <Login />
          </Route>
        </Switch>
      </Router>
